0d29954f071208e3025475d517f0e573f0a3249b
davinicollas <davi.nicollas@hotmail.com>
2021-05-03 15:24:20 -0300

    Static charts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from './App';
-import NavBar from 'components/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
